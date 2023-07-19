5c2bd976be4ef6b4e77800e587dde903aaaf4816
inbumbaek <inbumbaek@gmail.com>
2023-07-19 13:08:32 -0700

    card

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -110,3 +110,21 @@ const Dashboard = () => {
           </tbody>
         </table>
       </div>
+      {/* ----------CARD---------- */}
+      <div className="flex-container">
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src="../images/d-4.jpg" />
+
+              <p className="title">Brussel's Dwarf Hawaiian </p>
+              <p>Flip Me!</p>
+            </div>
+            <div className="flip-card-back">
+              <p>
+                Heptapleurum arboricola is a flowering plant in the family
+                Araliaceae, native to Taiwan and Hainan Province, China.
+              </p>
+            </div>
+          </div>
+        </div>
