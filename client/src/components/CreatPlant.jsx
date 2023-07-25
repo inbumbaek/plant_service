0dc59ca1b20d492aa152b3364d3c256cc3201fd8
inbumbaek <inbumbaek@gmail.com>
2023-07-25 14:26:11 -0700

    create submit

--- client/src/components/CreatPlant.jsx
+++ client/src/components/CreatPlant.jsx
@@ -10,3 +10,25 @@ const CreatePlant = () => {
   const [price, setPrice] = useState("");
   const [sellerName, setSellerName] = useState("");
   const [sellerEmail, setSellerEmail] = useState("");
+
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/api/plant", {
+        name,
+        lighting,
+        description,
+        price,
+        sellerName,
+        sellerEmail,
+      })
+      .then((response) => {
+        navigate("/dashboard");
+      })
+      .catch((err) => {
+        setErrors(err.response.data.err.errors);
+      });
+  };
