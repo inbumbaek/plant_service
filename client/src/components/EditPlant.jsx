a4efb865c184af7688c666ef19507fbe9aec5374
inbumbaek <inbumbaek@gmail.com>
2023-08-07 16:57:51 -0700

    description

--- client/src/components/EditPlant.jsx
+++ client/src/components/EditPlant.jsx
@@ -118,3 +118,18 @@ return (
                   <p className="error-message">{errors.lighting.message}</p>
                 ) : null}
               </div>
+              {/* <------------DESCRIPTION-----------> */}
+              <div className="mb-3">
+                <label htmlFor="description" className="form-label">
+                  Care Instructions & Info
+                </label>
+                <textarea
+                  className="form-control"
+                  onChange={(e) => setPlantDescription(e.target.value)}
+                  value={plantDescription}
+                  placeholder="Remember to give your plant the proper care it deserves."
+                />
+                {errors.description ? (
+                  <p className="error-message">{errors.description.message}</p>
+                ) : null}
+              </div>
