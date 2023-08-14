6c5e2191e512900958396fa3230f54458d6ec07a
inbumbaek <inbumbaek@gmail.com>
2023-08-13 23:39:11 -0700

    login

---
+++ client/src/components/Login.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
+
+const Login = (props) => {
+    const navigate = useNavigate()
+    const [userLogin, setUserLogin] = useState({
+        email:'',
+        password:''
+    })
