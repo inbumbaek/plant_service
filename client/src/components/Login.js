0d69ff71a44590916643eb6b121dfdf732e2c41b
inbumbaek <inbumbaek@gmail.com>
2023-08-14 10:29:44 -0700

    loginhandler

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,3 +12,16 @@ const Login = (props) => {
     const changeHandler = (e) => {
         setUserLogin({...userLogin, [e.target.name]:e.target.value})
     }
+
+    const loginHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/login', userLogin, {withCredentials:true})
+            .then((res) => {
+                console.log(res);
+                navigate('/dashboard')
+            })
+            .catch((err) => {
+                console.log(err);
+                // setErrors(err.response.data.errors)
+            })
+    }
