737fac0807c22e91b7d9264d28827b527f852d98
inbumbaek <inbumbaek@gmail.com>
2023-08-15 13:50:34 -0700

    login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,3 +25,21 @@ const Login = (props) => {
                 // setErrors(err.response.data.errors)
             })
     }
+
+    return (
+        <div className="login">
+            <h1 className="text-green-custom text-center">Welcome to Plant Care Service</h1>
+            <br />
+            <form onSubmit={loginHandler} className='col-4 mx-auto user-form'>
+                <div>
+                    <label className='form-label text-dark font-weight-bold'>Email: </label>
+                <input className='form-control' type='text' onChange={changeHandler} value={userLogin.email} name='email'/>
+                    {userLogin.email.length < 2 && userLogin.email.length > 0 ? (
+                    <p className="text-danger">Email must be at least 2 characters</p>
+                    ) : null}
+                <label className='form-label text-dark font-weight-bold'>Password: </label>
+                <input className='form-control' type='password' onChange={changeHandler} value={userLogin.password} name='password'/>
+                    {userLogin.password.length < 8 && userLogin.password.length > 0 ? (
+                    <p className="text-danger">Password be at least 8 characters</p>
+                    ) : null}
+                </div>
